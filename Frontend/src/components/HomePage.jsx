e9b184746c828ed2bbd354e54e6e1c136b0cb79c
dejiobasan <dejiobasan@yahoo.com>
2025-05-23 09:56:27 +0100

    Refactor code structure for improved readability and maintainability

--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Hero />
+      <About />
     </div>
   );
 };
